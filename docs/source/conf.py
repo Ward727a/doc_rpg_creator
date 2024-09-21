0888ddb0a71938155205a73f164988cf53ed85a5
Ward727 <ward727a@gmail.com>
2024-09-21 23:28:49 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RPG Creator'
+copyright = '2024, Ward727'
+author = 'Ward727'
 
 release = '0.1'
 version = '0.1.0'
